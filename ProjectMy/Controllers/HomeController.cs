b282ced8d292df07565fbada0fda3044af6905d4
NoumanLPS <nouman.tariq@logicpowered.net>
2017-04-10 15:14:54 +0500

    code added

---
+++ ProjectMy/Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProjectMy.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult MainMenu()
+        {
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+            List<String> names = new List<string>()
+            {
+                "Admin",
+                "Houshang",
+                "David",
+                "Paula",
+                "Lean",
+                "Ezima"
+            };
+            return View(names);
+        }
+
+        public ActionResult TakeAway()
+        {
+            return View();
+        }
+
+        public ActionResult ManagementSection()
+        {
+            return View();
+        }
+
+        public ActionResult DataFiles()
+        {
+            return View();
+        }
+
+        public ActionResult MenuItem()
+        {
+            return View();
+        }
+
+    }
+}
